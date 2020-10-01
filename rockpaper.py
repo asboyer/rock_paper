7183926a9c3b430b6fe20561b88ef11a766fa1c2
Andrew Boyer <asboyer@gmail.com>
2020-10-01 19:52:01 -0400

    v2

--- rockpaper.py
+++ rockpaper.py
@@ -7,25 +7,94 @@ def delay_print(s):
     for c in s:
         sys.stdout.write(c)
         sys.stdout.flush()
-        time.sleep(0.05)
+        time.sleep(0.004)
 
-name = str(input("What is your name: "))
+delay_print("What is your name: ")
+name = input()
+
 name = name.replace(' ','')
 
 if name == '':
-    name = 'Player_1'
-    
+    name = 'Player_One'    
 computer_score = 0
 player_score = 0
+rule = 0
+no = 0
+none = 0
+t = 0
+rules = '''
+Welcome to rockpaper.py, a simple game of rock paper scissors.
 
+The rules are simple. You will choose either a rock, paper, or scissors.
+Simultaneously, I will randomly choose a guess as well.
+Then we will compare.
+
+Scissors always beats paper. Paper always beats rock. Rock always beat scissors. 
+
+If you would like to play, please enter 'yes'. 
+If not, please respectfully enter a 'no', and be on your merry way.
+
+'''
+
+info = '''
+yes = play
+no = do not play
+
+Enter 'rules' if you would like to know the rules.
+
+'''
+
+delay_print("Hello, " + name + "!\nWould you like to play a game of rock, paper, scissor? ")
+choice = input() 
 while True:
     while True:
-        player = input("Rock, paper, or scissors? ")
+        if choice.lower() == 'yes' or choice.lower() == 'y':
+            break
+        elif choice.lower() == 'no' or choice.lower() == 'n':
+            delay_print("Too bad. Have a nice day, " + name + ".")
+            print("\n")
+            none = 1
+            break
+        elif choice.lower() == 'rules':
+            if rule == 0:
+                delay_print(rules)
+                rule = 1
+                choice = input()
+                continue
+            else:
+                print(rules)
+                choice = input()
+                continue
+        else:
+            if no < 2:
+                delay_print("Please give me a 'yes' or a 'no'. ")
+                choice = input()
+                no = no + 1
+                continue 
+            else:
+                delay_print(info)
+                choice = input()
+                continue
+    if none == 0:
+        pass
+    else:
+        break
+    while True:
+        if t == 0:
+            if sys.platform.startswith('win32'):
+                os.system('cls')
+            else:
+                os.system('clear')
+            t = 1
+        else:
+            print('\n')
+        delay_print("Rock, paper, or scissors? ")
+        player = input()
 
         if player.lower() == 'rock' or player.lower() == 'paper' or player.lower() == 'scissors' or player.lower() == 'r' or player.lower() == 'p' or player.lower() == 's' or player.lower() == 'scissor': 
             break
         else:
-            print("Please enter a valid input")
+            print("!Please enter a valid input!")
             continue
 
     if player.lower() == 'r':
@@ -37,8 +106,6 @@ while True:
     elif player.lower() == 'scissor':
         player = 'scissors'
 
-
-
     chosen = randint(1,3)
 
     if chosen == 1:
@@ -47,46 +114,39 @@ while True:
       chosen = 'paper'
     elif chosen == 3:
       chosen = 'scissors'
-
-    print(player.upper() + ' VS. ' + chosen.upper())
-
+    print("\n")
+    delay_print(player.upper() + ' VS. ' + chosen.upper())
+    print("\n")
+    
     if player == chosen:
       delay_print("DRAW!")
-      print(" ")
-
+      print(" \n")
     elif player == 'rock' and chosen == 'scissors':
       delay_print(name.upper() + ' WINS!')
-      print(" ")
+      print("\n")
       player_score = player_score + 1
     elif player == 'scissors' and chosen == 'paper':
       delay_print(name.upper() + ' WINS!')
-      print(" ")
+      print("\n")
       player_score = player_score + 1
     elif player == 'paper' and chosen == 'rock':
       delay_print(name.upper() + ' WINS!')
-      print(" ")
+      print("\n")
       player_score = player_score + 1
-    elif chosen == 'rock' and player == 'scissors':
-      delay_print(name.upper() + ' LOSES!')
-      computer_score = computer_score + 1
-      print(" ")
-    elif chosen == 'scissors' and player == 'paper':
-      delay_print(name.upper() + ' LOSES!')
-      computer_score = computer_score + 1
-      print(" ")
-    elif chosen == 'paper' and player == 'rock':
-      delay_print(name.upper() + ' LOSES!')
-      computer_score = computer_score + 1
-      print(" ")
+    else:
+        delay_print(name.upper() + ' LOSES!')
+        computer_score = computer_score + 1
+        print("\n")
     
     player_print_score = name + ': ' + str(player_score) 
     computer_print_score = "Computer: " + str(computer_score)
-    scorecard = player_print_score + '\n' + computer_print_score
+    scorecard = '\n' + player_print_score + '\n' + computer_print_score + '\n'
     
     print(scorecard)
     
     while True:
-        play = input("Play again? ")
+        delay_print("Would you like to play again? ")
+        play = input()
     
         if play.lower() == 'yes' or play.lower() == 'no':
             break
@@ -98,6 +158,9 @@ while True:
                 os.system('cls')
             else:
                 os.system('clear')
+        elif play.lower() == 'credit':
+            delay_print("\n\n'Written with love'\n\n\n- ballerboyer\n\n")
+            continue
         else:
             print("Please enter 'yes' or 'no'.") 
             continue
